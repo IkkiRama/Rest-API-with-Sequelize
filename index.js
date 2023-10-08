a4dfa9d97050092cbeb13c70843f7d50763b0255
IkkiRama <rifkivamaus@gmail.com>
2023-10-08 15:28:24 +0700

    Penyesuaian Port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const express = require("express") // ini perlu
+const express = require("express"); // ini perlu
 
 // init express server and router
 const app = express();
-const productRouter = require('./app/routes/product-routes');
-const transactionRouter = require('./app/routes/transaction-routes');
+const productRouter = require("./app/routes/product-routes");
+const transactionRouter = require("./app/routes/transaction-routes");
 
 app.use(express.json()); // supaya express bisa response json
 
@@ -12,6 +12,10 @@ app.use(express.json()); // supaya express bisa response json
 app.use("/api/products", productRouter);
 app.use("/api/transaction", transactionRouter);
 
-app.listen(process.env.APP_PORT, () => {
-    console.log("Express API running in port: " + process.env.APP_PORT);
-  });
+// Use PORT provided in environment or default to 3000
+const port = process.env.APP_PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", () => {
+  console.log("Express API running in port: " + process.env.APP_PORT);
+});
